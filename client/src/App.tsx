920d03ea5faac55bc39d5bfb1b364a219c35756c
Namachivayam2001 <namachivayam10419u25030@gmail.com>
2025-09-04 14:03:34 +0530

    Added Login page using matirial UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,28 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { CssVarsProvider, extendTheme } from '@mui/joy/styles';
+import Login from './pages/Login/Login';
+
+// ✅ Define theme without initialColorScheme
+const customTheme = extendTheme({
+  colorSchemes: {
+    light: {},
+    dark: {},
+  },
+});
 import './App.css'
 
 function App() {
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<h1>HOME</h1>} />
-      </Routes>
-    </Router>
-  )
+    return (
+        <CssVarsProvider theme={customTheme} defaultMode="light" disableTransitionOnChange>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<h1>HOME</h1>} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </Router>
+        </CssVarsProvider>
+    )
 }
 
 export default App
